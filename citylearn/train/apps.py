0d689f17c378190b64ed4f80afba21f56258e197
Steve Silva <silvasm516@aol.com>
2025-11-04 21:54:59 -0800

    app.py file

--- citylearn/train/apps.py
+++ citylearn/train/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class TrainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'train'
+    # name = 'train'
+    name = 'citylearn.application'

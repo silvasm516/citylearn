11d506b691c0476bcbb687bb645169d8552ad5ce
Steve Silva <silvasm516@aol.com>
2025-11-13 13:43:52 -0800

    req text chgs

--- citylearn/citylearn/settings.py
+++ citylearn/citylearn/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*=t2c@c8oe8f155=lzv8#!rb5+-!=tug0kz4hgb*r9&3j6xelq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '0.0.0.0']
 
 
 # Application definition
